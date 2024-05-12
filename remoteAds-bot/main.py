39e53f63d650ec22b9ac324ed41c5b44725abf42
AliValizade <alivalizade@gmail.com>
2024-05-13 02:45:12 +0330

    Add multi-language functionality

--- remoteAds-bot/main.py
+++ remoteAds-bot/main.py
@@ -19,18 +19,66 @@ def check_join(user, channels):
 
 @bot.message_handler(commands=['start'])
 def start(m):
-    markup = InlineKeyboardMarkup()
-    button = InlineKeyboardButton(text='ادامه', callback_data='proceed')
-    markup.add(button)
-    try:
-        with mysql.connector.connect(**db_config) as connection:
-            with connection.cursor() as cursor:
-                sql = f'INSERT INTO users (id) VALUES ({m.from_user.id})'
+    with mysql.connector.connect(**db_config) as connection:
+        with connection.cursor() as cursor:
+            sql = f"SELECT lang FROM users WHERE id = {m.from_user.id}"
+            print(sql)
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
+
+            if result is None:
+                sql = f"INSERT INTO users(id) VALUES ({m.from_user.id})"
                 cursor.execute(sql)
                 connection.commit()
-        bot.send_message(chat_id=m.chat.id, text='سلام کاربر جدید', reply_markup=markup)
-    except:
-        bot.send_message(chat_id=m.chat.id, text='سلام کاربر قدیمی', reply_markup=markup)
+
+                markup = InlineKeyboardMarkup(row_width=1)
+                button1 = InlineKeyboardButton(text='English', callback_data='en')
+                button2 = InlineKeyboardButton(text='فارسی', callback_data='fa')
+                markup.add(button1, button2)
+
+                bot.send_message(chat_id=m.chat.id, text='کاربر گرامی لطفا زبان خود را انتخاب کنید:\nPlease select your language:', reply_markup=markup)
+
+            else:
+                if result[0] == 'fa':
+                    bot.send_message(chat_id=m.chat.id, text='به ربات ما خوش آمدید')
+                else:
+                    bot.send_message(chat_id=m.chat.id, text='ٌWelcome to my bot')
+
+    # markup = InlineKeyboardMarkup()
+    # button = InlineKeyboardButton(text='ادامه', callback_data='proceed')
+    # markup.add(button)
+    # try:
+    #     with mysql.connector.connect(**db_config) as connection:
+    #         with connection.cursor() as cursor:
+    #             sql = f'INSERT INTO users (id) VALUES ({m.from_user.id})'
+    #             cursor.execute(sql)
+    #             connection.commit()
+    #     bot.send_message(chat_id=m.chat.id, text='سلام کاربر جدید', reply_markup=markup)
+    # except mysql.connector.Error as err:
+    #     print("خطای دیتابیس:", err)
+    #     bot.send_message(chat_id=m.chat.id, text='سلام کاربر قدیمی', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'en')
+def english(call):
+    with mysql.connector.connect(**db_config) as connection:
+        with connection.cursor() as cursor:
+            sql = f"UPDATE users SET lang = 'en' WHERE id = {call.from_user.id}"
+            cursor.execute(sql)
+            connection.commit()
+    bot.send_message(chat_id=call.message.chat.id, text='Your language is now English.')
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'fa')
+def english(call):
+    with mysql.connector.connect(**db_config) as connection:
+        with connection.cursor() as cursor:
+            sql = f"UPDATE users SET lang = 'fa' WHERE id = {call.from_user.id}"
+            cursor.execute(sql)
+            connection.commit()
+    bot.send_message(chat_id=call.message.chat.id, text='زبان شما به فارسی تغییر یافت.')
+    
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'proceed')
@@ -41,7 +89,7 @@ def proceed(call):
         markup = InlineKeyboardMarkup()
         button = InlineKeyboardButton(text='تایید', callback_data='proceed')
         markup.add(button)
-        bot.send_message(chat_id=call.message.chat.id, text='باید در کانال ما عضو شوید @Remote_project_bot')
+        bot.send_message(chat_id=call.message.chat.id, text='باید در کانال ما عضو شوید @Remote_ad , @Remote_ads')
     else:
         bot.send_message(chat_id=call.message.chat.id, text='شما میتوانید از ربات استفاده کنید')
 
