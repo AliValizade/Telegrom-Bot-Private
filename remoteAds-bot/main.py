3a08c14be19d877f11e3e976b5ffd502300e4d9f
AliValizade <alivalizade@gmail.com>
2024-05-13 00:57:35 +0330

    Forced to join

--- remoteAds-bot/main.py
+++ remoteAds-bot/main.py
@@ -33,6 +33,19 @@ def start(m):
         bot.send_message(chat_id=m.chat.id, text='سلام کاربر قدیمی', reply_markup=markup)
 
 
+@bot.callback_query_handler(func=lambda call: call.data == 'proceed')
+def proceed(call):
+    is_member = check_join(user=call.from_user.id, channels=channels)
+
+    if is_member is False:
+        markup = InlineKeyboardMarkup()
+        button = InlineKeyboardButton(text='تایید', callback_data='proceed')
+        markup.add(button)
+        bot.send_message(chat_id=call.message.chat.id, text='باید در کانال ما عضو شوید @Remote_project_bot')
+    else:
+        bot.send_message(chat_id=call.message.chat.id, text='شما میتوانید از ربات استفاده کنید')
+
+
 
 if __name__ == "__main__":
     bot.remove_webhook()
